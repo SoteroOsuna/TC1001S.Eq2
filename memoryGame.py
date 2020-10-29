685192ff82e114694e18891c00c61fbac257c59a
SoteroOsuna <a01251505@itesm.mx>
2020-10-29 14:11:57 -0600

    Despliega número de taps

--- memoryGame.py
+++ memoryGame.py
@@ -4,8 +4,13 @@ from freegames import path
 
 car = path('car.gif')
 tiles = list(range(32)) * 2
-state = {'mark': None}
+#Turtle responsable de escribir el número de taps
+writer = Turtle(visible=False)
+state = {'mark': None, "taps": 0}
 hide = [True] * 64
+
+def inside(x, y):
+    return -200 < x < 200 and -200 < y < 200
 
 def square(x, y):
     "Draw white square with black outline at (x, y)."
@@ -29,18 +34,27 @@ def xy(count):
 
 def tap(x, y):
     "Update mark and hidden tiles based on tap."
-    spot = index(x, y)
-    mark = state['mark']
+    if(inside(x, y)):
+        spot = index(x, y)
+        mark = state['mark']
 
-    if mark is None or mark == spot or tiles[mark] != tiles[spot]:
-        state['mark'] = spot
-    else:
-        hide[spot] = False
-        hide[mark] = False
-        state['mark'] = None
+        if mark is None or mark == spot or tiles[mark] != tiles[spot]:
+            state['mark'] = spot
+        else:
+            hide[spot] = False
+            hide[mark] = False
+            state['mark'] = None
+
+        global tapCounter
+        state["taps"] += 1
+        print(state["taps"])
 
 def draw():
     "Draw image and tiles."
+
+    #Escribir el número de taps en la pantalla
+    writer.undo()
+    writer.write(state["taps"], font=("Arial", 30, 'bold'), align='center')
     clear()
     goto(0, 0)
     shape(car)
@@ -56,7 +70,7 @@ def draw():
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
-        goto(x + 2, y)
+        goto(x, y)
         color('black')
         write(tiles[mark], font=('Arial', 30, 'normal'))
 
@@ -64,10 +78,14 @@ def draw():
     ontimer(draw, 100)
 
 shuffle(tiles)
-setup(420, 420, 370, 0)
+setup(500, 500, 370, 0)
+tracer(False)
+writer.goto(0, 210)
+writer.color('blue')
+writer.write(state["taps"], font=("Arial", 30, 'bold'), align='center')
+listen()
 addshape(car)
 hideturtle()
-tracer(False)
 onscreenclick(tap)
 draw()
 done()
